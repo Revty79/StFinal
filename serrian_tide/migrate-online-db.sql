da2d441332f451c6acaa1fd42872d48831939c73
Revty79 <serriantide@gmail.com>
2025-11-30 08:18:35 -0700

    massive project cleanup lol

--- serrian_tide/migrate-online-db.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE campaigns ADD COLUMN IF NOT EXISTS starting_credits integer DEFAULT 0 NOT NULL;
-
-ALTER TABLE campaign_currencies ALTER COLUMN credit_value TYPE numeric(10, 4) USING credit_value::numeric;
--- serrian_tide/migrations/0005_add_is_setup_complete.sql
+++
@@ -1,3 +0,0 @@
--- Add is_setup_complete column to campaign_characters table
-ALTER TABLE campaign_characters 
-ADD COLUMN is_setup_complete BOOLEAN NOT NULL DEFAULT false;
